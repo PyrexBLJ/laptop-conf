423bed3da29d222a8661cb444b5f001db0ad5eaf
PyrexBLJ <49227284+PyrexBLJ@users.noreply.github.com>
2025-08-08 19:15:13 -0400

    really fix configs and reorganize

--- post_install.sh
+++ post_install.sh
@@ -8,17 +8,7 @@ sudo groupadd input
 
 sudo usermod -a -G input $USER
 
-git clone https://aur.archlinux.org/yay.git
-
-cd yay
-
-makepkg -si
-
-cd ~/.config
-
-mkdir -v fuzzel kitty niri waybar systemd systemd/user
-
-cp -r ~/Downloads/laptop-conf/.config ~/.config
+cp -rv ~/Downloads/laptop-conf/.config ~
 
 systemctl --user daemon-reload
 systemctl --user add-wants niri.service swaybg.service
@@ -26,5 +16,11 @@ systemctl --user add-wants niri.service swaybg.service
 killall waybar && waybar & disown
 
 flatpak install floorp discord gpu_screen_recorder
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
 
 yay -S visual-studio-code-bin
