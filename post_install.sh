49e6bc482ec23b82b2a8a7a5b4845b392da9a8a2
PyrexBLJ <49227284+PyrexBLJ@users.noreply.github.com>
2025-08-08 16:02:50 -0400

    create dirs for configs
    so they actually get copied over lol

--- post_install.sh
+++ post_install.sh
@@ -2,13 +2,21 @@
 
 cd ~
 
-sudo pacman -S ttf-jetbrains-mono-nerd power-profiles-daemon flatpak base-devel kitty thunar ark nsxiv -y
+sudo pacman -S --needed ttf-jetbrains-mono-nerd power-profiles-daemon flatpak base-devel kitty thunar ark nsxiv xwayland-satellite git
+
+sudo groupadd input
+
+sudo usermod -a -G $USER input
 
 git clone https://aur.archlinux.org/yay.git
 
 cd yay
 
 makepkg -si
+
+cd ~/.config
+
+mkdir -v fuzzel kitty niri waybar systemd systemd/user
 
 cp -r ~/Downloads/laptop-conf/.config ~/.config
 
